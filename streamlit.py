31f549aba8b740b6f5c93bd77e8593ebb1c00341
Edward Raigosa <edward.raigosa@snowflake.com>
2023-02-05 21:59:22 -0800

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -1,3 +1,25 @@
+# starter demo app
+# import streamlit as st
+# x = st.slider('x')
+# st.write(x, 'squared is', x * x)
+
 import streamlit as st
-x = st.slider('x')
-st.write(x, 'squared is', x * x)
+from gsheetsdb import connect
+
+# Create a connection object.
+conn = connect()
+
+# Perform SQL query on the Google Sheet.
+# Uses st.cache to only rerun when the query changes or after 10 min.
+@st.cache(ttl=600)
+def run_query(query):
+    rows = conn.execute(query, headers=1)
+    rows = rows.fetchall()
+    return rows
+
+sheet_url = st.secrets["public_gsheets_url"]
+rows = run_query(f'SELECT * FROM "{sheet_url}"')
+
+# Print results.
+for row in rows:
+    st.write(f"{row.name} has a :{row.pet}:")
